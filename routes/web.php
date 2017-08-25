5b565a9cef61d0b1279d75f4afd5a5fc0787c922
tthanhkhoa <tthanhkhoa@gmail.com>
2017-08-26 04:51:52 +0700

    Update

--- routes/web.php
+++ routes/web.php
@@ -124,5 +124,5 @@ Route::get('/lichsubanhang', function () {
 
 Route::get('/', 'Client\SanPhamController@getPageHome')->name('home');
 
-Route::get('/', 'Client\SanPhamController@getPageHome')->name('home');
+Route::get('/home', 'Client\SanPhamController@getPageHome')->name('home');
 
