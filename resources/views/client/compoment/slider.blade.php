84bda4857b71ca313024ae2bdb2be72aac72431d
miceli <khoatran@kerofrog.com>
2017-08-26 03:27:09 +0700

    update

--- resources/views/client/compoment/slider.blade.php
+++ resources/views/client/compoment/slider.blade.php
@@ -37,7 +37,7 @@
                                     </div>
                                     <div class="col-sm-6">
                                         <img src="{{isset($item->image_url) ? $item->image_url : ''}}" class="girl img-responsive" alt="" />
-                                        <img src="images/home/pricing.png"  class="pricing" alt="" />
+                                        {{--<img src="images/home/pricing.png"  class="pricing" alt="" />--}}
                                     </div>
                                 </div>
                             @endif
