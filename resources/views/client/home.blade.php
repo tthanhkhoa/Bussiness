4bb93ebf2cd1219c65a9584f2e0c17d5e8576352
miceli <khoatran@kerofrog.com>
2017-08-27 01:20:10 +0700

    update link get image chitietsanpham

--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -7,16 +7,16 @@
 <div class="features_items"><!--features_items-->
     <h2 class="title text-center">Các sản phẩm nổi bật</h2>
     @foreach($sanpham as $item)
-    <div class="col-sm-4">
-        <div class="product-image-wrapper">
-            <div class="single-products">
+    <div class="col-sm-4" >
+        <div class="product-image-wrapper" >
+            <div class="single-products" >
                 <div class="productinfo text-center">
                     <a href="{{route('chitietsanpham',['id'=>$item->id])}}">
                         <img  width="80" height="180" alt="80x180"
                               src="{{isset($item->Images->first()->image_url) ? $item->Images->first()->image_url : asset('images/defaut/S2B.jpg')}}"  />
-                        <h2>{{isset($item->giatien) ? number_format($item->giatien) : '0' }}</h2>
-                        <p>{{isset($item->tensanpham) ? $item->tensanpham : 'S2B Beauty'}}</p>
-                        <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Thêm vào giỏ hàng</a>
+                        <h2 >{{isset($item->giatien) ? number_format($item->giatien) : '0' }} VNĐ</h2>
+                        <p style="display: inline;" >{{isset($item->tensanpham) ? $item->tensanpham : 'S2B Beauty'}}</p>
+                        <a href="#" class="btn btn-default add-to-cart" style="float:right;"><i style="display: inline;" class="fa fa-shopping-cart"></i>Thêm vào giỏ hàng</a>
                     </a>
 
                 </div>
@@ -30,11 +30,6 @@
         </div>
     </div>
    @endforeach
-
-
-
-
-
 </div>
 
 
