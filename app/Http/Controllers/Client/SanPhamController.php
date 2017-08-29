9fc806ce63e0836b8b971b9f063a67acac3a92b3
miceli <khoatran@kerofrog.com>
2017-08-29 11:10:31 +0700

    no message

--- app/Http/Controllers/Client/SanPhamController.php
+++ app/Http/Controllers/Client/SanPhamController.php
@@ -57,6 +57,7 @@ class SanPhamController extends Controller
         //$data = \Location::get($getIP);
         $response= file_get_contents('http://ip-api.com/json/'.$getIP);
         $data = json_decode($response);
+        $data = json_encode($data);
         $result = new MyStruct();
         $result->as = $data->as;
         $result->city = $data->city;
